688522e963acecaddf1cb49d84c4514da1b8eb9f
AhmadAwab98 <149583996+AhmadAwab98@users.noreply.github.com>
2023-11-10 16:13:24 +0500

    Learned Control Structures
    Learned
    - If, else statements
    - For loop
    - Switch statement

--- Control_Structure/main.go
+++ Control_Structure/main.go
@@ -22,20 +22,27 @@ func Sqrt(x float64) float64 {
 
 func grade(marks int) {
 	switch gmarks := marks / 10; gmarks {
+	// grade is A+ if marks in between 90 and 100
 	case 10:
 		fmt.Println("Your grade is A+")
 	case 9:
 		fmt.Println("Your grade is A+")
+	// grade is A if marks in between 80 and 89
 	case 8:
 		fmt.Println("Your grade is A")
+	// grade is B+ if marks in between 70 and 79
 	case 7:
 		fmt.Println("Your grade is B+")
+	// grade is B if marks in between 60 and 69
 	case 6:
 		fmt.Println("Your grade is B")
+	// grade is C if marks in between 50 and 59
 	case 5:
-		fmt.Println("Your grade is D+")
+		fmt.Println("Your grade is C")
+	// grade is D if marks in between 40 and 49
 	case 4:
 		fmt.Println("Your grade is D")
+	// fail if marks less than 40
 	default:
 		fmt.Println("You failed")	
 	}
