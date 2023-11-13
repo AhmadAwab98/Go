d3095992ef61e50c640a032e0f3f7e323d0ce665
AhmadAwab98 <149583996+AhmadAwab98@users.noreply.github.com>
2023-11-13 12:58:29 +0500

    Learned basics
     Learned
    - Variable types
    - Type conversion and inference
    - Functions

--- Variables_and_Functions/main.go
+++ Variables_and_Functions/main.go
@@ -5,33 +5,33 @@ import (
 	"math"
 )
 
-func isubtract_two(a, b int) int {
+func subtractTwo(a, b int) int {
 
     return a - b
 }
 
-func iadd_three(a, b, c int) int {
+func addThree(a, b, c int) int {
     return a + b + c
 }
 
-func pythagoras_theorem(a, b int) (c float64) {
+func pythagorasTheorem(a, b int) (c float64) {
 	// convert a and b to float64
 	d, e := float64(a), float64(b)
 	//computing c
-	c := math.Sqrt(d*d + e*e)
+	c = math.Sqrt(d*d + e*e)
 	return
 }
 
 func main() {
 	x, y, z := 27, 9, 18
 	// subtract two int type variables 
-	subtraction_int := isubtract_two(x, y)
+	subtractionInt := subtractTwo(x, y)
 	fmt.Println("27 - 9 =", subtraction_int)
 	// add three int type variables
-	addition_int := iadd_three(x, y, z)
+	additionInt := addThree(x, y, z)
 	fmt.Println("27 + 9 + 18 =", addition_int)
 	
 	// compute hypotenuse using pythagoras theorem
-	c := pythagoras_theorem(x,y)
+	c := pythagorasTheorem(x,y)
 	fmt.Println("Hypotenuse of given triangle is:", c)
 }
